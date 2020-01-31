1ee806cb79dbfd3fa62738c2b5fdff6a2bba7892
Artyom Nekhoroshev <a.nekhoroshev@uiscom.ru>
2020-01-31 22:21:32 +0300

    app routing

--- src/components/clients/ClientEditPage.js
+++ src/components/clients/ClientEditPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-
-export default function ClientEditPage() {
+export default function ClientEditPage(props) {
+    console.log(props);
     return (
         <div>
-            <h3>- Client Edit Page </h3>
+            <h3> - Client Edit Page </h3>
+            <div> {props.match.params.id} </div>
         </div>
     );
 }
